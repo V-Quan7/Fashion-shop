34cc3370ab4f6cd607e7bf4dc8b5bca25715cf39
vn-Quan <vn.quan.dev@gmail.com>
2026-05-07 15:37:05 +0700

    frontend fix

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5001/api"
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const getProducts = async (params = {}) => {
     const queryParams = new URLSearchParams();
